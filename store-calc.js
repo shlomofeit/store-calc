e671bfa675276d8d41e51b6db6c9204790083491
shlomofeit <11.5sfeit@gmail.com>
2026-07-26 16:18:11 +0300

    Added calculateDiscount function

--- store-calc.js
+++ store-calc.js
@@ -1,4 +1,6 @@
-export function calculateDiscount(price, percent) {}
+export function calculateDiscount(price, percent) {
+  return (price * (100 - percent)) / 100;
+}
 
 export function applyTax(price, percent) {}
 
@@ -7,3 +9,5 @@ export function calculateCartTotal(items) {}
 export function validatePrice(price) {}
 
 export function formatPrice(amount) {}
+
+console.log(calculateDiscount(100, 20));
